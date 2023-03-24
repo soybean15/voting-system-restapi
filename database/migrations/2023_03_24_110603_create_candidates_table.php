921045a6002c6a16b05d9dee4057060aaf75f040
soybean15 <marlonpadilla1593@gmail.com>
2023-03-24 19:33:37 +0800

    initial commit

---
+++ database/migrations/2023_03_24_110603_create_candidates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('vote_count');
+            $table->bigInteger('position_id')->unsigned()->index();
+            $table->foreign('position_id')->references('id')->on('positions')->onDelete('cascade');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('candidates');
+    }
+};
