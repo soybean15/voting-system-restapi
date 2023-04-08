40c25b3dbfe5936efdefa0bbedfd402b997841ae
soybean15 <marlonpadilla1593@gmail.com>
2023-04-08 14:40:39 +0800

    FINALLY WORKING AUTH

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CandidateController;
-use App\Http\Controllers\PartyListController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', function () {
-    return view('homepage');
+    return ['Laravel' => app()->version()];
 });
-// Route::resource('candidate', CandidateController::class);
 
-// Route::resource('candidate', CandidateController::class);
-// Route::post('candidate/store',  [CandidateController::class,'storeCandidate'])->name('candidate.store');
-Route::middleware(['cors'])->group(function () {
-    Route::resource('candidate', CandidateController::class);
-    Route::resource('partylist', PartyListController::class);
-
-  //  Route::resource('candidate', CandidateController::class);
-    Route::post('candidate/store',  [CandidateController::class,'storeCandidate'])->name('candidate.store');
-    Route::get('candidate/position/{id}/add',[CandidateController::class,'createCandidate'])->name('candidate.add');
-    Route::get('candidate/update',  [CandidateController::class,'update'])->name('candidate.update');
-    Route::delete('candidate/position/{id}',  [CandidateController::class,'destroyPosition'])->name('candidate.destroyposition');
-    Route::get('/position/{id}/edit',[CandidateController::class,'editPosition'])->name('position.edit');
-    Route::post('candidate/position/{id}',  [CandidateController::class,'updatePosition'])->name('position.update');
-});
+require __DIR__.'/auth.php';
