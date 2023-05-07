2998af0886bd7be1083e26ab2b85d9dec5040930
soybean15 <marlonpadilla1593@gmail.com>
2023-05-07 23:12:50 +0800

    modify admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Candidate;
+use App\Models\Position;
+use App\Models\PartyList;
 
 class AdminController extends Controller
 {
@@ -21,12 +24,18 @@ class AdminController extends Controller
 
     public function dashboard(){
 
-        $partylist = \App\Models\PartyList::all();
+        $partylist_count = \App\Models\PartyList::count();
+        $candidate_count = Candidate::count();
+        $position_count = Position::count();
+        $user_count = \App\Models\User::count();
 
 
         return response()->json([
             "status" => 1,
-            "partylist"=>$partylist
+            "partylist_count"=>$partylist_count,
+            "candidate_count" =>$candidate_count,
+            "position_count"=> $position_count,
+            "user_count"=> $user_count
 
         ]);
     }
