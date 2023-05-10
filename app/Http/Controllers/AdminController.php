d09ad0ffc2c5cff65fc49d2fa9d2de439f9227b0
soybean15 <marlonpadilla1593@gmail.com>
2023-05-10 21:04:07 +0800

    modified dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\Candidate;
 use App\Models\Position;
 use App\Models\PartyList;
+use App\Models\User;
+use App\Models\VoteLog;
 
 class AdminController extends Controller
 {
@@ -23,19 +25,27 @@ class AdminController extends Controller
     }
 
     public function dashboard(){
+        $positions= Position::with('candidates')->get();
 
         $partylist_count = \App\Models\PartyList::count();
         $candidate_count = Candidate::count();
-        $position_count = Position::count();
+        $position_count = $positions->count();
         $user_count = \App\Models\User::count();
 
+        // $hasVoted = User::has('voteLog')->get();
+        // $notVoted = User::doesntHave('vote_log')->get();
 
+        $voteLogs = VoteLog::with('user')
+            ->orderBy('created_at')
+            ->get();
         return response()->json([
             "status" => 1,
             "partylist_count"=>$partylist_count,
             "candidate_count" =>$candidate_count,
             "position_count"=> $position_count,
-            "user_count"=> $user_count
+            "user_count"=> $user_count,
+            "positions"=>$positions,
+            "vote_logs"=>$voteLogs
 
         ]);
     }
