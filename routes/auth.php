60ab062a7bd5789dca095c788e251434eb1c4f09
soybean15 <marlonpadilla1593@gmail.com>
2023-05-15 22:47:23 +0800

    test  email verification

--- routes/auth.php
+++ routes/auth.php
@@ -25,7 +25,7 @@ Route::post('/reset-password', [NewPasswordController::class, 'store'])
                 ->name('password.store');
 
 Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
-                ->middleware(['auth', 'signed', 'throttle:6,1'])
+                ->middleware(['auth', 'signed', 'throttle:60,1'])
                 ->name('verification.verify');
 
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
