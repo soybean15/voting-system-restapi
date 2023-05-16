f4cac9d094f2c3adf0c057a8c466dfa188121721
soybean15 <marlonpadilla1593@gmail.com>
2023-05-17 01:05:16 +0800

    modify emailverificaition controlle

--- routes/auth.php
+++ routes/auth.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
-
+use App\Providers\RouteServiceProvider;
 Route::post('/register', [RegisteredUserController::class, 'store'])
                 ->middleware('guest')
                 ->name('register');
