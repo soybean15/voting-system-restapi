24dab1cbb8b43346436c64bc15dd09158167fb69
soybean15 <marlonpadilla1593@gmail.com>
2023-05-16 22:13:26 +0800

    modify emailverificaition controller

--- routes/auth.php
+++ routes/auth.php
@@ -24,8 +24,8 @@ Route::post('/reset-password', [NewPasswordController::class, 'store'])
                 ->middleware('guest')
                 ->name('password.store');
 
-Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class,'__invoke')
-                ->middleware([ 'signed', 'throttle:6,1'])
+Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+                ->middleware(['auth', 'signed', 'throttle:6,1'])
                 ->name('verification.verify');
 
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
