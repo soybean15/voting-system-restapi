a8fca982c37a7667594ae35633c97d7bf7dd73c8
soybean15 <marlonpadilla1593@gmail.com>
2023-05-16 21:30:04 +0800

    modify emailverificaition controller

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -15,32 +15,19 @@ class VerifyEmailController extends Controller
      */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
-        // if ($request->user()->hasVerifiedEmail()) {
-        //     return redirect()->intended(
-        //         config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1'
-        //     );
-        // }
-
-        // if ($request->user()->markEmailAsVerified()) {
-        //     event(new Verified($request->user()));
-        // }
-       
-        // return redirect()->intended(
-        //     config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1'
-        // );
-
-        $userId = $request->route('id');
-        $verificationToken = $request->route('hash');
-
-        $user = User::find($userId);
-
-        if ($user && hash_equals($verificationToken, $user->getEmailVerificationToken())) {
-            $user->markEmailAsVerified();
-            event(new Verified($user));
-            return redirect()->intended(config('app.frontend_url') . RouteServiceProvider::HOME . '?verified=1');
-        } else {
-            // Verification failed
-            return redirect()->intended(config('app.frontend_url') . RouteServiceProvider::HOME . '?verified=0');
+        if ($request->user()->hasVerifiedEmail()) {
+            return redirect()->intended(
+                config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1'
+            );
         }
+
+        if ($request->user()->markEmailAsVerified()) {
+            event(new Verified($request->user()));
+        }
+       
+        return redirect()->intended(
+            config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1'
+        );
+
     }
 }
